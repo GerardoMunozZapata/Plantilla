632c77a30584531e1c5605deb48d6e29e348bcb5
Gerardo Munoz Zapata <gerardo.munoz.zapata@gmail.com>
2015-08-11 00:10:10 -0500

    Plantillas Commit

---
+++ app/views/includes/head.blade.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="author" content="Plantilla">
+<link rel="icon" href="{{ URL::asset('assets/img/SinoSky_Favicon.png') }}">
+
+<title>Plantillas|</title>
+
+<!-- load bootstrap from a cdn -->
+<link rel="stylesheet" href="aseets/css/bootstrap.min.css">
---
+++ app/views/includes/header.blade.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+    <div class="navbar-inner">
+        <a id="logo" href="{{URL::to('/')}}">Ejemplo Plantilla</a>
+        <ul class="nav">
+            <li><a href="{{URL::to('/')}}">Inicio</a></li>
+            <li><a href="{{URL::to('/about')}}">Acerca</a></li>
+            <li><a href="{{URL::to('/projects')}}">Proyectos</a></li>
+            <li><a href="{{URL::to('/contact')}}">Contacto</a></li>
+        </ul>
+    </div>
+</div>
